013b335a9fcd23dce100ade70152379c35421745
Alyson Consuli <consulialyson@gmail.com>
2022-05-06 13:52:59 -0300

    feat: encriptar senha

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import { MongoClient } from 'mongodb'
 import dotenv from 'dotenv'
 import joi from 'joi'
+import bcrypt from 'bcrypt'
 
 const app = express()
 app.use(cors())
@@ -16,9 +17,13 @@ promise.then(() => db = mongoClient.db('my-wallet'))
 
 app.post('/users', async (req, res) => {
     try {
-        const user = req.body
-        await db.collection('users').insertOne(user)
-        //console.log(await db.collection('users').find({}).toArray())
+        const { name, email, password } = req.body
+        const passwordHash = bcrypt.hashSync(password, 10);
+        await db.collection('users').insertOne({
+            name,
+            email,
+            password: passwordHash
+        })
         res.sendStatus(201)
     } catch {
         console.log('Erro ao cadastrar usuário')
@@ -28,17 +33,17 @@ app.post('/users', async (req, res) => {
 
 app.post('/login', async (req, res) => {
     try {
-        const {email, password} = req.body
-        const user = await db.collection('users').findOne({
-            email,
-            password
-        })
-        if(!user){
+        const { email, password } = req.body
+        const user = await db.collection('users').findOne({ email })
+        if (!user) {
             return res.sendStatus(404)
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+            return res.sendStatus(401)
         }
         res.send(user).status(200)
     } catch {
-        console.log('Erro ao cadastrar usuário')
+        console.log('Erro ao fazer login')
         res.sendStatus(500)
     }
 })
