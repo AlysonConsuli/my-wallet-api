61dd252ff42a4e44506a0740647d87949336a0c1
Alyson Consuli <consulialyson@gmail.com>
2022-05-06 16:56:21 -0300

    feat: post & get items]

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ app.post('/users', async (req, res) => {
         await db.collection('users').insertOne({
             name,
             email,
-            password: passwordHash
+            password: passwordHash,
+            items: []
         })
         res.sendStatus(201)
     } catch {
@@ -50,6 +51,59 @@ app.post('/login', async (req, res) => {
         res.send({ ...user, token }).status(200)
     } catch {
         console.log('Erro ao fazer login')
+        res.sendStatus(500)
+    }
+})
+
+app.get('/items', async (req, res) => {
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "").trim();
+    if (!token) {
+        return res.sendStatus(401);
+    }
+    const session = await db.collection('sessions').findOne({ token });
+    if (!session) {
+        return res.sendStatus(401);
+    }
+
+    try {
+        const user = await db.collection("users").findOne({ _id: session.userId });
+        res.status(201).send(user.items);
+
+    } catch {
+        console.log('Erro ao pegar os itens')
+        res.sendStatus(500)
+    }
+})
+
+app.post('/items', async (req, res) => {
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "").trim();
+    if (!token) {
+        return res.sendStatus(401);
+    }
+    const session = await db.collection('sessions').findOne({ token });
+    if (!session) {
+        return res.sendStatus(401);
+    }
+    try {
+        const { value, description, type, date } = req.body
+        const user = await db.collection("users").findOne({ _id: session.userId });
+        await db.collection("users").updateOne({
+            _id: session.userId
+        }, {
+            $push: {
+                items: {
+                    value: Number(value),
+                    description,
+                    type,
+                    date
+                }
+            }
+        })
+        res.sendStatus(201)
+    } catch {
+        console.log('Erro ao postar o item')
         res.sendStatus(500)
     }
 })
