9ee9f82d0fec484622d8b008d2f17506986b82b3
Alyson Consuli <consulialyson@gmail.com>
2022-05-07 17:00:03 -0300

    feat: validaçoes post users

--- src/middlewares/userSchemaMidlleware.js
+++ src/middlewares/userSchemaMidlleware.js
@@ -1,11 +1,20 @@
+import db from "../db.js"
 import { userSchema } from "../schemas/userSchema.js"
 
-export const userSchemaMidlleware = (req, res, next) => {
+export const userSchemaMidlleware = async (req, res, next) => {
     const validation = userSchema.validate(req.body, { abortEarly: false })
     if (validation.error) {
         console.log(validation.error.details.map(detail => detail.message))
         return res.sendStatus(422)
     }
-
-    next()
+    try {
+        const { email } = req.body
+        const conflict = await db.collection('users').findOne({ email })
+        if (conflict) {
+            return res.sendStatus(409)
+        }
+        next()
+    } catch {
+        res.sendStatus(500)
+    }
 }
--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -3,5 +3,6 @@ import joi from "joi"
 export const userSchema = joi.object({
     name: joi.string().required(),
     email: joi.string().email().required(),
-    password: joi.string().required()
+    password: joi.string().required().pattern(new RegExp('^[a-zA-Z0-9]{3,}$')),
+    repeatPassword: joi.ref('password')
 })
